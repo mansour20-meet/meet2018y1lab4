cd0153735a0f8125be78fda9813c5290854a1129
mansour20-meet <mansour20@meet.mit.edu>
2018-08-01 07:30:09 -0400

    Stopping here

---
+++ indentation.py
@@ -0,0 +1,11 @@
+indentation = False
+
+if indentation:
+    print('chocolate')
+print('Indentations are cool!')
+
+indentation = True
+
+if indentation:
+    print('chocolate')
+print('Indentations are cool!')
